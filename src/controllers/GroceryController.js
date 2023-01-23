445a361b2be0885f1c4962c364a726dba4b81823
Bruno Guimarães <mguimaraesbruno@gmail.com>
2023-01-23 12:27:35 -0300

    feat: return grocery object when updating

--- src/controllers/GroceryController.js
+++ src/controllers/GroceryController.js
@@ -1,75 +1,96 @@
-const { Grocery } = require('../models');
+const { Grocery } = require("../models");
 
 module.exports = {
-
-    index(req, res) {
-        Grocery.findAll({})
-            .then(groceries => res.json({
-                error: false,
-                data: groceries
-            }))
-            .catch(error => res.json({
-                error:true,
-                data: [],
-                error: error
-            }));
-    },
-
-    create(req, res) {
-        const { name } = req.body;
-        Grocery.create({
-            name, checked: false
+  index(req, res) {
+    Grocery.findAll({})
+      .then((groceries) =>
+        res.json({
+          error: false,
+          data: groceries,
         })
-        .then(grocery => res.status(201).json({
+      )
+      .catch((error) =>
+        res.json({
+          error: true,
+          data: [],
+          error: error,
+        })
+      );
+  },
+
+  create(req, res) {
+    const { name } = req.body;
+    Grocery.create({
+      name,
+      checked: false,
+    })
+      .then((grocery) =>
+        res.status(201).json({
+          error: false,
+          data: grocery,
+          message: "new grocery has been created",
+        })
+      )
+      .catch((error) =>
+        res.json({
+          error: true,
+          data: [],
+          error: error,
+        })
+      );
+  },
+
+  update(req, res) {
+    const grocery_id = req.params.id;
+    const { name, checked } = req.body;
+
+    try {
+      Grocery.update(
+        {
+          name,
+          checked,
+        },
+        {
+          where: {
+            id: grocery_id,
+          },
+        }
+      ).then(() => {
+        Grocery.findByPk(grocery_id).then((grocery) =>
+          res.status(201).json({
             error: false,
             data: grocery,
-            message: "new grocery has been created"
-        }))
-        .catch(error => res.json({
-            error:true,
-            data: [],
-            error: error
-        }));
-    },
-
-    update(req, res) {
-        const grocery_id = req.params.id;
-
-        const { name, checked } = req.body;
-console.log(' req =>', req)
-console.log(' res =>', res)
-console.log('=>', {grocery_id, name, checked})
-        Grocery.update({
-            name, checked
-        }, {
-            where: {
-                id: grocery_id
-            }
-        })
-        .then(grocery => res.status(201).json({
-            error: false,
-            data: grocery,
-            message: 'grocery has been updated'
-        }))
-        .catch(error => res.json({
-            error: true,
-            error: error
-        }));
-    },
-
-    destroy(req, res) {
-        const grocery_id = req.params.id;
-
-        Grocery.destroy({ where: {
-            id: grocery_id
-        }})
-        .then(status => res.status(201).json({
-            error: false,
-            message: 'grocery has been deleted'
-        }))
-        .catch(error => res.json({
-            error: true,
-            error: error
-        }));
+            message: "grocery has been updated",
+          })
+        );
+      });
+    } catch (err) {
+      return res.json({
+        error: true,
+        error: err,
+      });
     }
-}
+  },
+
+  destroy(req, res) {
+    const grocery_id = req.params.id;
+
+    Grocery.destroy({
+      where: {
+        id: grocery_id,
+      },
+    })
+      .then((status) =>
+        res.status(201).json({
+          error: false,
+          message: "grocery has been deleted",
+        })
+      )
+      .catch((error) =>
+        res.json({
+          error: true,
+          error: error,
+        })
+      );
+  },
+};
